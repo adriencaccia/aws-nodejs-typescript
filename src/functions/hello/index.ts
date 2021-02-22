3df1947097d6cb1dd8216ed0dd4f337a47ca2ac3
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2021-02-22 15:57:45 +0100

    chore(eslint): fix errors and warnings

--- src/functions/hello/index.ts
+++ src/functions/hello/index.ts
@@ -1,7 +1,10 @@
 import schema from './schema';
 
 export default {
-  handler: `${__dirname.split(process.cwd())[1].substring(1).replace(/\\/g, '/')}/handler.main`,
+  handler: `${__dirname
+    .split(process.cwd())[1]
+    .substring(1)
+    .replace(/\\/g, '/')}/handler.main`,
   events: [
     {
       http: {
@@ -9,10 +12,10 @@ export default {
         path: 'hello',
         request: {
           schema: {
-            'application/json': schema
-          }
-        }
-      }
-    }
-  ]
-}
+            'application/json': schema,
+          },
+        },
+      },
+    },
+  ],
+};
--- src/libs/apiGateway.ts
+++ src/libs/apiGateway.ts
@@ -1,12 +1,28 @@
-import type { APIGatewayProxyEvent, APIGatewayProxyResult, Handler } from "aws-lambda"
-import type { FromSchema } from "json-schema-to-ts";
+import type {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Handler,
+} from 'aws-lambda';
+import type { FromSchema } from 'json-schema-to-ts';
 
-type ValidatedAPIGatewayProxyEvent<S> = Omit<APIGatewayProxyEvent, 'body'> & { body: FromSchema<S> }
-export type ValidatedEventAPIGatewayProxyEvent<S> = Handler<ValidatedAPIGatewayProxyEvent<S>, APIGatewayProxyResult>
+type ValidatedAPIGatewayProxyEvent<S> = Omit<APIGatewayProxyEvent, 'body'> & {
+  body: FromSchema<S>;
+};
+export type ValidatedEventAPIGatewayProxyEvent<S> = Handler<
+  ValidatedAPIGatewayProxyEvent<S>,
+  APIGatewayProxyResult
+>;
 
-export const formatJSONResponse = (response: Record<string, unknown>) => {
+interface Response {
+  statusCode: number;
+  body: string;
+}
+
+export const formatJSONResponse = (
+  response: Record<string, unknown>,
+): Response => {
   return {
     statusCode: 200,
-    body: JSON.stringify(response)
-  }
-}
+    body: JSON.stringify(response),
+  };
+};
