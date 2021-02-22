99446ccc45c82aa6e6ddf3714c6150d854af8bde
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2021-02-22 16:56:47 +0100

    feat: make source-map-support automatic

--- webpack.config.js
+++ webpack.config.js
@@ -32,21 +32,9 @@ module.exports = {
   externals: [nodeExternals()],
   module: {
     rules: [
-      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
       {
         test: /\.(tsx?)$/,
-        loader: 'ts-loader',
-        exclude: [
-          [
-            path.resolve(__dirname, 'node_modules'),
-            path.resolve(__dirname, '.serverless'),
-            path.resolve(__dirname, '.webpack'),
-          ],
-        ],
-        options: {
-          transpileOnly: true,
-          experimentalWatchApi: true,
-        },
+        loader: 'babel-loader',
       },
     ],
   },
